84ae3b7de559c82d1bf86199c5f7addcb7cdad00
roopesh90 <roopesh90nair@live.com>
2016-01-12 12:16:38 +0530

    base script, now lets c if this works

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,15 @@
 """Basic script for Fizz Buzz : http://c2.com/cgi/wiki?FizzBuzzTest
 """
+
+def check_fizz_buzz(num=0):
+    FB=""
+    if num%3 == 0:
+        FB += "Fizz"
+    if num%5 == 0:
+        FB += "Buzz"
+    return FB
+
+for i in range(1,100):
+    print(i)
+    IS_FB = check_fizz_buzz(i)
+    print("%s\n" % (IS_FB))
